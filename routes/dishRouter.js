76dcd1d345a00893ec341dd89b146b65d5087933
Abhinav Singh <singhabhinav.270@gmail.com>
2017-10-20 18:35:43 +0530

    Modified dishRouter.js file

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -10,13 +10,13 @@ dishRouter.route('/')
     .all((req, res, next) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-    
+
         next();
     })
 
 
     .get((req, res, next) => {
-        res.end('will send all the dishes to you ..Get Ready...stay tuned!');
+        res.end('Will send all the dishes to you ..Get Ready...stay tuned!');
     })
 
 
